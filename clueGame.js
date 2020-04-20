858e4649add3e2cf55f77ce39166536d9d0a5a06
brandon.tran <brandon.tran@servicenow.com>
2020-04-19 17:57:57 -0700

    added background images

---
+++ clueGame.js
@@ -0,0 +1,38 @@
+//Player Functions
+function isPlayersTurn() {
+
+}
+
+function movementAction() {
+
+}
+
+function movementAllowed() {
+
+}
+
+function makeAccusation() {
+
+}
+
+function win() {
+
+}
+
+
+//Room Functions
+function getCoordinates() {
+
+}
+
+function isRoom() {
+
+}
+
+function isOccupied() {
+
+}
+
+function hasSecretPassage() {
+
+}
